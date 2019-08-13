0907cb0287079ce7c633ec6d8cb1d91dd712c77c
Mironov Stas <stasmironovweb@gmail.com>
2019-08-13 12:20:25 +0300

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
-  resources :microposts
+
   resources :users
+
+  #resources :microposts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
