df9284fb3f54a08b02634be4adf7436ea7a63f70
Mironov Stas <stasmironovweb@gmail.com>
2019-08-14 17:57:29 +0300

    test

--- db/migrate/20190814135010_create_microposts.rb
+++ db/migrate/20190814135010_create_microposts.rb
@@ -1,10 +1,12 @@
 class CreateMicroposts < ActiveRecord::Migration[5.2]
   def change
+    drop_table :microposts
     create_table :microposts do |t|
       t.text :content
-      t.integer :user_id
+      t.references :user, foreign_key: true
 
       t.timestamps
+      add_index :microposts, [:user_id, :created_at]
     end
   end
 end
